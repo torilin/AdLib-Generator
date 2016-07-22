3f577e855b8423e85a9ad85dbb72b3272a6e9527
torilin <torilinsf@gmail.com>
2016-07-21 21:17:11 -0700

    Created Function for the Main TextField that takes in inputs from UserInput Textfield.

--- AdLib Generator/ViewController.swift
+++ AdLib Generator/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    var textFields: [UITextField]!
+    
     @IBOutlet weak var pastTenseVerbTextField: UITextField!
     @IBOutlet weak var singularNounTextField: UITextField!
     @IBOutlet weak var pluralNounTextField: UITextField!
@@ -24,14 +26,36 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        textFields = [pastTenseVerbTextField, singularNounTextField, pluralNounTextField, enterPhraseTextField]
     
     }
 
 
-    @IBAction func sliderValueChanged(sender: AnyObject) {
+    @IBAction func sliderValueChanged(sender: UISlider) {
+    
+        sliderLabel.text = "\(Int(numberSlider.value))"
+    
     }
     @IBAction func createButtonPressed(sender: AnyObject) {
+    
+        var location = ""
+        
+        if locationSegmentControl.selectedSegmentIndex == 0 {
+            location = "mall"
+        } else if locationSegmentControl.selectedSegmentIndex == 1 {
+            location = "library"
+        } else {
+            location = "restaurant"
+        }
+        
+        finishSentence.text = "One evening, at the  \(location) a \(singularNounTextField.text!) \(sliderLabel.text!) \(pluralNounTextField.text!) and said, \(enterPhraseTextField.text!)"
+        
     }
 
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        <#code#>
+    }
+    
 }
 
