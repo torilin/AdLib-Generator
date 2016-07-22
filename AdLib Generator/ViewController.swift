6272f875cf0ced1631d4dd7c39d1a1f092939c94
torilin <torilinsf@gmail.com>
2016-07-21 20:43:29 -0700

    Added Labels + Outlets and hooked it up to UI View

--- AdLib Generator/ViewController.swift
+++ AdLib Generator/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pastTenseVerbTextField: UITextField!
+    @IBOutlet weak var singularNounTextField: UITextField!
+    @IBOutlet weak var pluralNounTextField: UITextField!
+    @IBOutlet weak var enterPhraseTextField: UITextField!
+
+    @IBOutlet weak var locationSegmentControl: UISegmentedControl!
+    @IBOutlet weak var sliderLabel: UILabel!
+    @IBOutlet weak var numberSlider: UISlider!
+    
+    @IBOutlet weak var createAdLibs: UIButton!
+    @IBOutlet weak var finishSentence: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    
-    }
 
+    @IBAction func sliderValueChanged(sender: AnyObject) {
+    }
+    @IBAction func createButtonPressed(sender: AnyObject) {
+    }
 
 }
 
